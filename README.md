1e44a33d01f7c1449b6d81320b04d274b9cfbc6c
Maxim Borer <maxxborer@gmail.com>
2024-01-30 23:59:15 +0600

    add bun.sh

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-**The benchmarks are now available [on the pnpm website](https://r.pnpm.io/benchmarks).**
-
-**The code that generates the benchmarks was moved [here](https://github.com/pnpm/pnpm.github.io/tree/main/benchmarks)**.
-
-***
-
 # Benchmarks of JavaScript Package Managers
 
-This benchmark compares the performance of [npm](https://github.com/npm/cli), [pnpm](https://github.com/pnpm/pnpm) and [Yarn](https://github.com/yarnpkg/yarn) (both regular and PnP variant).
+This benchmark compares the performance of [npm](https://github.com/npm/cli), [pnpm](https://github.com/pnpm/pnpm), [Yarn](https://github.com/yarnpkg/yarn) and [Bun](https://bun.sh/).
 
 Here's a quick explanation of how these tests could apply to the real world:
 
@@ -20,20 +14,20 @@ Here's a quick explanation of how these tests could apply to the real world:
 - `with node_modules`: The package cache and the lockfile is deleted and the install command is run again.
 - `update`: Updating your dependencies by changing the version in the `package.json` and running the install command again.
 
-## Lots of Files
+## Angular App
 
-The app's `package.json` [here](./fixtures/alotta-files/package.json)
+The app's `package.json` [here](./fixtures/angular-quickstart/package.json)
 
-| action  | cache | lockfile | node_modules| npm | pnpm | Yarn | Yarn PnP |
+| action  | cache | lockfile | node_modules| npm | pnpm | Yarn | Yarn PnP | Bun |
 | ---     | ---   | ---      | ---         | --- | --- | --- | --- |
-| install |       |          |             | 51s | 14.4s | 39.1s | 29.1s |
-| install | ✔     | ✔        | ✔           | 5.4s | 1.3s | 707ms | n/a |
-| install | ✔     | ✔        |             | 10.9s | 3.9s | 11s | 1.8s |
-| install | ✔     |          |             | 33.4s | 6.5s | 26.5s | 17.2s |
-| install |       | ✔        |             | 28.3s | 11.8s | 23.3s | 14.2s |
-| install | ✔     |          | ✔           | 4.6s | 1.7s | 22.1s | n/a |
-| install |       | ✔        | ✔           | 6.5s | 1.3s | 713ms | n/a |
-| install |       |          | ✔           | 6.1s | 5.4s | 41.1s | n/a |
-| update  | n/a   | n/a      | n/a         | 5.1s | 10.7s | 35.4s | 28.3s |
+| install |       |          |             | 1m 20.4s | 21.6s | 30.9s | 23.7s | 5.9s |
+| install | ✔     | ✔        | ✔           | 8.5s | 591ms | 272ms | n/a | 314ms |
+| install | ✔     | ✔        |             | 5.8s | 6.3s | 6.1s | 942ms | 1s |
+| install | ✔     |          |             | 8.7s | 21s | 14.7s | 9.9s | 1.1s |
+| install |       | ✔        |             | 15.2s | 15.6s | 20.1s | 14.1s | 1.1s |
+| install | ✔     |          | ✔           | 1.8s | 846ms | 10.2s | n/a | 320ms |
+| install |       | ✔        | ✔           | 4.7s | 748ms | 329ms | n/a | 113ms |
+| install |       |          | ✔           | 5s | 5s | 28.6s | n/a | 91ms |
+| update  | n/a   | n/a      | n/a         | 1.7s | 14.9s | 23.5s | 17.9s | 108ms |
 
-![Graph of the alotta-files results](./results/imgs/alotta-files.svg)
+![Graph of the angular-quickstart results](./results/imgs/angular-quickstart.svg)
--- index.js
+++ index.js
@@ -12,26 +12,26 @@ const generateSvg = require('./lib/generateSvg')
 const LIMIT_RUNS = 3
 
 const fixtures = [
-  /*{
-    name: 'react-app',
-    mdDesc: '## React App\n\nThe app\'s `package.json` [here](./fixtures/react-app/package.json)'
-  },
-  {
-    name: 'ember-quickstart',
-    mdDesc: '## Ember App\n\nThe app\'s `package.json` [here](./fixtures/ember-quickstart/package.json)'
-  },
+  // {
+  //   name: 'react-app',
+  //   mdDesc: '## React App\n\nThe app\'s `package.json` [here](./fixtures/react-app/package.json)'
+  // },
+  // {
+  //   name: 'ember-quickstart',
+  //   mdDesc: '## Ember App\n\nThe app\'s `package.json` [here](./fixtures/ember-quickstart/package.json)'
+  // },
   {
     name: 'angular-quickstart',
     mdDesc: '## Angular App\n\nThe app\'s `package.json` [here](./fixtures/angular-quickstart/package.json)'
   },
-  {
-    name: 'medium-size-app',
-    mdDesc: '## Medium Size App\n\nThe app\'s `package.json` [here](./fixtures/medium-size-app/package.json)'
-  },*/
-  {
-    name: 'alotta-files',
-    mdDesc: '## Lots of Files\n\nThe app\'s `package.json` [here](./fixtures/alotta-files/package.json)'
-  }
+  // {
+  //   name: 'medium-size-app',
+  //   mdDesc: '## Medium Size App\n\nThe app\'s `package.json` [here](./fixtures/medium-size-app/package.json)'
+  // },
+  // {
+  //   name: 'alotta-files',
+  //   mdDesc: '## Lots of Files\n\nThe app\'s `package.json` [here](./fixtures/alotta-files/package.json)'
+  // }
 ]
 
 const tests = [
@@ -102,42 +102,44 @@ run()
   .catch(err => console.error(err))
 
 async function run () {
-  const pms = [ 'npm', 'pnpm', 'yarn', 'yarn_pnp' ]
+  const pms = [ 'npm', 'pnpm', 'yarn', 'yarn_pnp', 'bun' ]
   const sections = []
   const svgs = []
   for (const fixture of fixtures) {
     const npmRes = min(await benchmark(cmdsMap.npm, fixture.name, {limitRuns: LIMIT_RUNS, hasNodeModules: true}))
     const yarnRes = min(await benchmark(cmdsMap.yarn, fixture.name, {limitRuns: LIMIT_RUNS, hasNodeModules: true}))
     const yarnPnPRes = min(await benchmark(cmdsMap.yarn_pnp, fixture.name, {limitRuns: LIMIT_RUNS, hasNodeModules: false}))
-    const pnpmRes = min(await benchmark(cmdsMap.pnpm, fixture.name, {limitRuns: LIMIT_RUNS, hasNodeModules: true}))
+    const pnpmRes = min(await benchmark(cmdsMap.pnpm, fixture.name, { limitRuns: LIMIT_RUNS, hasNodeModules: true }))
+    const bunRes = min(await benchmark(cmdsMap.bun, fixture.name, { limitRuns: LIMIT_RUNS, hasNodeModules: true }))
     const resArray = toArray(pms, {
       'npm': npmRes,
       'pnpm': pnpmRes,
       'yarn': yarnRes,
       'yarn_pnp': yarnPnPRes,
+      'bun': bunRes
     })
 
     sections.push(stripIndents`
       ${fixture.mdDesc}
 
-      | action  | cache | lockfile | node_modules| npm | pnpm | Yarn | Yarn PnP |
+      | action  | cache | lockfile | node_modules| npm | pnpm | Yarn | Yarn PnP | Bun |
       | ---     | ---   | ---      | ---         | --- | --- | --- | --- |
-      | install |       |          |             | ${prettyMs(npmRes.firstInstall)} | ${prettyMs(pnpmRes.firstInstall)} | ${prettyMs(yarnRes.firstInstall)} | ${prettyMs(yarnPnPRes.firstInstall)} |
-      | install | ✔     | ✔        | ✔           | ${prettyMs(npmRes.repeatInstall)} | ${prettyMs(pnpmRes.repeatInstall)} | ${prettyMs(yarnRes.repeatInstall)} | n/a |
-      | install | ✔     | ✔        |             | ${prettyMs(npmRes.withWarmCacheAndLockfile)} | ${prettyMs(pnpmRes.withWarmCacheAndLockfile)} | ${prettyMs(yarnRes.withWarmCacheAndLockfile)} | ${prettyMs(yarnPnPRes.withWarmCacheAndLockfile)} |
-      | install | ✔     |          |             | ${prettyMs(npmRes.withWarmCache)} | ${prettyMs(pnpmRes.withWarmCache)} | ${prettyMs(yarnRes.withWarmCache)} | ${prettyMs(yarnPnPRes.withWarmCache)} |
-      | install |       | ✔        |             | ${prettyMs(npmRes.withLockfile)} | ${prettyMs(pnpmRes.withLockfile)} | ${prettyMs(yarnRes.withLockfile)} | ${prettyMs(yarnPnPRes.withLockfile)} |
-      | install | ✔     |          | ✔           | ${prettyMs(npmRes.withWarmCacheAndModules)} | ${prettyMs(pnpmRes.withWarmCacheAndModules)} | ${prettyMs(yarnRes.withWarmCacheAndModules)} | n/a |
-      | install |       | ✔        | ✔           | ${prettyMs(npmRes.withWarmModulesAndLockfile)} | ${prettyMs(pnpmRes.withWarmModulesAndLockfile)} | ${prettyMs(yarnRes.withWarmModulesAndLockfile)} | n/a |
-      | install |       |          | ✔           | ${prettyMs(npmRes.withWarmModules)} | ${prettyMs(pnpmRes.withWarmModules)} | ${prettyMs(yarnRes.withWarmModules)} | n/a |
-      | update  | n/a   | n/a      | n/a         | ${prettyMs(npmRes.updatedDependencies)} | ${prettyMs(pnpmRes.updatedDependencies)} | ${prettyMs(yarnRes.updatedDependencies)} | ${prettyMs(yarnPnPRes.updatedDependencies)} |
+      | install |       |          |             | ${prettyMs(npmRes.firstInstall)} | ${prettyMs(pnpmRes.firstInstall)} | ${prettyMs(yarnRes.firstInstall)} | ${prettyMs(yarnPnPRes.firstInstall)} | ${prettyMs(bunRes.firstInstall)} |
+      | install | ✔     | ✔        | ✔           | ${prettyMs(npmRes.repeatInstall)} | ${prettyMs(pnpmRes.repeatInstall)} | ${prettyMs(yarnRes.repeatInstall)} | n/a | ${prettyMs(bunRes.repeatInstall)} |
+      | install | ✔     | ✔        |             | ${prettyMs(npmRes.withWarmCacheAndLockfile)} | ${prettyMs(pnpmRes.withWarmCacheAndLockfile)} | ${prettyMs(yarnRes.withWarmCacheAndLockfile)} | ${prettyMs(yarnPnPRes.withWarmCacheAndLockfile)} | ${prettyMs(bunRes.withWarmCacheAndLockfile)} |
+      | install | ✔     |          |             | ${prettyMs(npmRes.withWarmCache)} | ${prettyMs(pnpmRes.withWarmCache)} | ${prettyMs(yarnRes.withWarmCache)} | ${prettyMs(yarnPnPRes.withWarmCache)} | ${prettyMs(bunRes.withWarmCache)} |
+      | install |       | ✔        |             | ${prettyMs(npmRes.withLockfile)} | ${prettyMs(pnpmRes.withLockfile)} | ${prettyMs(yarnRes.withLockfile)} | ${prettyMs(yarnPnPRes.withLockfile)} | ${prettyMs(bunRes.withLockfile)} |
+      | install | ✔     |          | ✔           | ${prettyMs(npmRes.withWarmCacheAndModules)} | ${prettyMs(pnpmRes.withWarmCacheAndModules)} | ${prettyMs(yarnRes.withWarmCacheAndModules)} | n/a | ${prettyMs(bunRes.withWarmCacheAndModules)} |
+      | install |       | ✔        | ✔           | ${prettyMs(npmRes.withWarmModulesAndLockfile)} | ${prettyMs(pnpmRes.withWarmModulesAndLockfile)} | ${prettyMs(yarnRes.withWarmModulesAndLockfile)} | n/a | ${prettyMs(bunRes.withWarmModulesAndLockfile)} |
+      | install |       |          | ✔           | ${prettyMs(npmRes.withWarmModules)} | ${prettyMs(pnpmRes.withWarmModules)} | ${prettyMs(yarnRes.withWarmModules)} | n/a | ${prettyMs(bunRes.withWarmModules)} |
+      | update  | n/a   | n/a      | n/a         | ${prettyMs(npmRes.updatedDependencies)} | ${prettyMs(pnpmRes.updatedDependencies)} | ${prettyMs(yarnRes.updatedDependencies)} | ${prettyMs(yarnPnPRes.updatedDependencies)} | ${prettyMs(bunRes.updatedDependencies)} |
 
       ![Graph of the ${fixture.name} results](./results/imgs/${fixture.name}.svg)
     `)
 
     svgs.push({
       path: join(__dirname, 'results', 'imgs', `${fixture.name}.svg`),
-      file: generateSvg(resArray, [cmdsMap.npm, cmdsMap.pnpm, cmdsMap.yarn, cmdsMap.yarn_pnp], testDescriptions)
+      file: generateSvg(resArray, [cmdsMap.npm, cmdsMap.pnpm, cmdsMap.yarn, cmdsMap.yarn_pnp, cmdsMap.bun], testDescriptions)
     })
   }
 
@@ -147,7 +149,7 @@ async function run () {
   const introduction = stripIndents`
   # Benchmarks of JavaScript Package Managers
 
-  This benchmark compares the performance of [npm](https://github.com/npm/cli), [pnpm](https://github.com/pnpm/pnpm) and [Yarn](https://github.com/yarnpkg/yarn) (both regular and PnP variant).
+  This benchmark compares the performance of [npm](https://github.com/npm/cli), [pnpm](https://github.com/pnpm/pnpm), [Yarn](https://github.com/yarnpkg/yarn) and [Bun](https://bun.sh/).
   `
 
   const explanation = stripIndents`
